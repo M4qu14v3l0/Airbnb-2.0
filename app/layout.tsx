29bf4d4010bdc0c8726cde97b48b6cee9a785519
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2023-02-18 10:29:44 -0500

    first page ended

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import './globals.css'
 
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body>
       <Header />
         {children}
+      <Footer />
       </body>
       {/* footer */}
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Inter } from '@next/font/google'
 import Banner from './components/Banner/Banner'
 import ExploreNearby from './components/ExploreNearby/ExploreNearby'
+import LargeCard from './components/LargeCard/LargeCard'
 import LiveAyWhere from './components/LiveAnyWhere/LiveAnyWhere'
 
 
@@ -13,6 +14,13 @@ export default function Home() {
       <div className='max-w-7xl mx-auto px-8 sm:px-16'>
         <ExploreNearby />
         <LiveAyWhere />
+        <LargeCard 
+        
+          img='https://links.papareact.com/4cj'
+          title='The Greatest Outdoors'
+          description='Whislists curated by Airbnb'
+          buttonText='Get Inspired'
+        />
       </div>
     </main>
   )
